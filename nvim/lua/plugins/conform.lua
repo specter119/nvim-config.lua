db0f6811529c1a1b225d936ec05a371e5d6a9476
specter119 <specter119@gmail.com>
2025-06-18 13:31:00 +0800

    Adds format-on-save configuration for Conform.nvim
    Introduces format-on-save settings, including a fallback for LSP formatting and a timeout threshold of 500ms. Enhances code formatting automation for supported file types.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,13 @@
 return {
   'stevearc/conform.nvim',
-  opt = {
+  opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff_organize_imports', 'ruff_fix', 'ruff_format' },
+    },
+    format_on_save = {
+      lsp_format = 'fallback',
+      timeout_ms = 500,
     },
   },
 }
